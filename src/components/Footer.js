e18d986787d348a837410162c6e03145e7ad7739
Chris Ware <c.p.ware@hotmail.com>
2023-07-13 11:28:36 -0700

    Stripe connect + seeded products

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,8 @@
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="border-t border-gray-100 py-10 text-center">
+      <p className="text-sm text-gray-500">copy right info here</p>
+    </footer>
   )
 }
 export default Footer
