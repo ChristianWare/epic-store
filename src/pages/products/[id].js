d1b1f5aa2f13569611c2699b79c121fa48f80129
Chris Ware <c.p.ware@hotmail.com>
2023-07-13 15:59:31 -0700

    Started the product details page

---
+++ src/pages/products/[id].js
@@ -0,0 +1,84 @@
+import Image from "next/image";
+import { stripe } from "src/utils/stripe";
+import { formatCurrencyString } from "use-shopping-cart";
+
+const ProductPage = ({ product }) => {
+  return (
+    <div className='container lg:max-w-screen-lg mx-auto py-12 px-6'>
+      <div className='felx flex-col md:flex-row justify-between items-center space-y-8 md:space-x-12'>
+        <div className='relative w-72 h-72 sm:w-96 sm:h-96'>
+          <Image
+            src={product.image}
+            alt={product.name}
+            fill
+            objectFit='contain'
+            sizes='100%'
+            priority
+          />
+        </div>
+        <h2>{product.name}</h2>
+        <p>In Stock</p>
+        <p>
+          Price:{" "}
+          {formatCurrencyString({
+            value: product.price,
+            currency: product.currency,
+          })}
+        </p>
+
+        <div className='mt-4 border-t pt-4'>
+          <p>Quantity:</p>
+          <div className='mt-1 flex items-center space-x-3'>
+            <button className='p-1 rounded-md hover:bg-rose-100 hover:text-rose-500 w-6 h-6 text-4xl'>
+              -
+            </button>
+            <p className='text-2xl'>0</p>
+            <button className='p-1 rounded-md hover:bg-green-100 hover:text-green-500 w-6 h-6 text-4xl'>
+              +
+            </button>
+          </div>
+        </div>
+        <button className="mt-4 border border-lime-500 px-6 py-2 rounded-md">Add to Cart</button>
+      </div>
+    </div>
+  );
+};
+export default ProductPage;
+
+export async function getStaticPaths() {
+  const inventory = await stripe.products.list();
+  const paths = inventory.data.map((product) => ({
+    params: { id: product.id },
+  }));
+
+  return {
+    paths,
+    fallback: "blocking",
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const inventory = await stripe.products.list({
+    expand: ["data.default_price"],
+  });
+
+  const products = inventory.data.map((product) => {
+    const price = product.default_price;
+    return {
+      currency: price.currency,
+      id: product.id,
+      name: product.name,
+      price: price.unit_amount,
+      image: product.images[0],
+    };
+  });
+
+  const product = products.find((product) => product.id === params.id);
+
+  return {
+    props: {
+      product,
+    },
+    revalidate: 60 * 60,
+  };
+}
